62116cbaf2c717c1f94775f2399c7e3420c511f1
RGBCube <RGBCube@users.noreply.github.com>
2022-12-16 22:36:03 +0300

    Rename test

--- src/time_test.v
+++ src/time_test.v
@@ -2,7 +2,7 @@ module revolt
 
 import time
 
-fn test_time() {
+fn test_time_format() {
 	time := Time(time.unix(1670178420))
 	assert time.format(.short_time) == '<t:1670178420:t>'
 	assert time.format(.long_time) == '<t:1670178420:T>'
