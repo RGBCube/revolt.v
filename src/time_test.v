32e7c79cef6ac3a12a22cccb70d11b90a3234285
RGBCube <RGBCube@users.noreply.github.com>
2022-12-16 22:35:00 +0300

    Add time test

---
+++ src/time_test.v
@@ -0,0 +1,14 @@
+module revolt
+
+import time
+
+fn test_time() {
+	time := Time(time.unix(1670178420))
+	assert time.format(.short_time) == '<t:1670178420:t>'
+	assert time.format(.long_time) == '<t:1670178420:T>'
+	assert time.format(.short_date) == '<t:1670178420:d>'
+	assert time.format(.long_date) == '<t:1670178420:D>'
+	assert time.format(.short_date_time) == '<t:1670178420:f>'
+	assert time.format(.long_date_time) == '<t:1670178420:F>'
+	assert time.format(.relative) == '<t:1670178420:R>'
+}
